01ad92e6919375c9e23eec61bf8eed06d6b2e37c
asim-developer <asimzaheer296@gmail.com>
2025-03-15 23:03:31 +0500

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,10 +1,10 @@
 function showAd(num) {
     console.log("Clicked on: " + num); // Debugging
 
-    // Hide all ads
+
     document.querySelectorAll('.ad').forEach(ad => ad.style.display = 'none');
 
-    // Show the selected ad
+
     let adElement = document.querySelector('.ad' + num);
     if (adElement) {
         adElement.style.display = "block";
